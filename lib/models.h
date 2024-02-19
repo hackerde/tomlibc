1eb7b0787a0ea771ce582b8991408b557d71b919
Anway De <anway1756@gmail.com>
2024-02-19 22:27:39 +0000

    documentation: lib part 1

--- lib/models.h
+++ lib/models.h
@@ -13,6 +13,11 @@
 #define MAX_STRING_LENGTH   512
 #define MAX_ARRAY_LENGTH    50
 
+/*
+    Enum `value_type` represents the set of value types
+    accepted by this TOML parser. This corresponds to
+    the various types defined in the TOML Spec.
+*/
 typedef enum value_type value_type_t;
 enum value_type
 {
@@ -26,38 +31,67 @@ enum value_type
     TIMELOCAL,
     ARRAY,
     INLINETABLE,
-    GARBAGE,
 };
 
+/*
+    Struct `value` holds the various attributes associated
+    with a TOML value.
+*/
 typedef struct value value_t;
 struct value
 {
+    /* the type of TOML value */
     value_type_t    type;
+    /* used for storing `ARRAY` type values */
     value_t**       arr;
+    /* used for storing non-`ARRAY` type values */
     void*           data;
+    /* used for printing numeric values */
     size_t          precision;
     bool            scientific;
+    /* used for printing datetime values */
     char*           format;
 };
 
+/*
+    Enum `key_type` represents the various types of
+    keys that this TOML parser is aware of. It is used
+    to make re-defining validity decisions.
+*/
 typedef enum key_type key_type_t;
 enum key_type
 {
+    /* `[a.b]` -> a */
     TABLE,
+    /* `j.k = v` -> j */
     KEY,
+    /* `[[t]]` -> t */
     ARRAYTABLE,
+    /* `[a.b]` -> b */
     TABLELEAF,
+    /* `j.k = v` -> k */
     KEYLEAF,
 };
 
+/*
+    Struct `key` defines the TOML keys. Each node in the
+    parsed AST is a `key`, irrespective of the fact if
+    they were defined as TOML keys or tables.
+*/
 typedef struct key key_t;
 struct key
 {
+    /* key type as described above */
     key_type_t  type;
+    /* identifier */
     char        id[ MAX_ID_LENGTH ];
+    /* list of subkeys */
     key_t*      subkeys[ MAX_NUM_SUBKEYS ];
+    /* pointer to the last added subkey */
     key_t**     last;
+    /* value associated with this key */
     value_t*    value;
+    /* used for indexing ARRAYTABLES */
     size_t      idx;
 };
 
--- lib/utils.h
+++ lib/utils.h
@@ -3,23 +3,45 @@
 
 #include <stdio.h>
 
+/*
+    Macro `LOG_ERR` is for logging an error
+    message to stderr, that also specifies
+    which file, line and function the error
+    was raised in.
+*/
 #define LOG_ERR( ... )                      \
     { fprintf( stderr, "%s:%d [%s]: ",      \
     __FILE__, __LINE__, __func__ );         \
     fprintf( stderr, __VA_ARGS__ ); }
 
+/*
+    Macro `LOG_ERR_BREAK` calls `LOG_ERR`
+    and then calls a `break;`
+*/
 #define LOG_ERR_BREAK( ... )                \
     { LOG_ERR( __VA_ARGS__ );               \
     break; }
 
+/*
+    Macro `LOG_ERR_RETURN` calls `LOG_ERR`
+    and then returns NULL.
+*/
 #define LOG_ERR_RETURN( ... )               \
     { LOG_ERR( __VA_ARGS__ );               \
     return NULL; }
 
+/*
+    Macro `FAIL_BREAK` calls `LOG_ERR_BREAK`
+    if the given condition fails.
+*/
 #define FAIL_BREAK( COND, ... )             \
     if( !( COND ) )                         \
         LOG_ERR_BREAK( __VA_ARGS__ );       \
 
+/*
+    Macro `FAIL_RETURN` calls `LOG_ERR_RETURN`
+    if the given condition fails.
+*/
 #define FAIL_RETURN( COND, ... )            \
     if( !( COND ) )                         \
         LOG_ERR_RETURN( __VA_ARGS__ );      \
