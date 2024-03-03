a10111ed078551b9abfa9ddb7204bef5bdf28404
Anway De <32722936+hackerde@users.noreply.github.com>
2024-03-02 19:33:04 -0600

    add regression report (#5)

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,6 +7,8 @@ help()
     echo "          Type of tests to run: valid or invalid"
     echo "      -m <PATTERN> --match <PATTERN>"
     echo "          Match test filenames to PATTERN"
+    echo "      -r --regression"
+    echo "          Calculate and show regression report"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -19,6 +21,10 @@ while [[ $# -gt 0 ]]; do
         -m|--match)
             MATCH=$2
             shift
+            shift
+            ;;
+        -r|--regression)
+            REGRESSION=1
             shift
             ;;
         -h|--help)
@@ -41,8 +47,8 @@ if [[ ! -z $TYPE && "$TYPE" != "valid" && "$TYPE" != "invalid" ]]; then
     exit 1
 fi
 
-PASSED=0
-FAILED=0
+PASSED=()
+FAILED=()
 TOTAL=0
 
 RED='\033[0;31m'
@@ -52,9 +58,12 @@ NC='\033[0m'
 
 DIR=$(realpath $(realpath $0)/../..)
 LOG=$DIR/tests/test.log
+OLD_STATUS=$DIR/tests/tomlibc.status
+NEW_STATUS=$OLD_STATUS.new
 BINARY=$DIR/tests/test
 
 cd $DIR
+> $NEW_STATUS
 make test > $LOG
 
 cd $DIR/tests
@@ -80,8 +89,14 @@ if [[ -z $TYPE || "$TYPE" == "valid" ]]; then
         json="${test%.toml}.json"
         diff <(jq --sort-keys . $json 2>>$LOG) <(jq --sort-keys . $JSON 2>>$LOG) >> $LOG
         r=$?
-        test $r -eq 0 && PASSED=$(( PASSED+1 )) || FAILED=$(( FAILED+1 ))
-        test $r -eq 0 && printf "[${GREEN}PASSED${NC}]: ${test}\n" || printf "[${RED}FAILED${NC}]: ${test}\n"
+        if [[ $r -eq 0 ]]; then
+            PASSED+=( $test )
+            printf "[${GREEN}PASSED${NC}]: ${test}\n"
+            echo $test >> $NEW_STATUS
+        else
+            FAILED+=( $test )
+            printf "[${RED}FAILED${NC}]: ${test}\n"
+        fi
         echo >> $LOG
     done
 fi
@@ -99,15 +114,65 @@ if [[ -z $TYPE || "$TYPE" == "invalid" ]]; then
         echo >> $LOG
         $BINARY $test >> $LOG 2>/dev/null
         r=$?
-        test $r -eq 1 && PASSED=$(( PASSED+1 )) || FAILED=$(( FAILED+1 ))
-        test $r -eq 1 && printf "[${GREEN}PASSED${NC}]: ${test}\n" || printf "[${RED}FAILED${NC}]: ${test}\n"
+        if [[ $r -eq 1 ]]; then
+            PASSED+=( $test )
+            printf "[${GREEN}PASSED${NC}]: ${test}\n"
+            echo $test >> $NEW_STATUS
+        else
+            FAILED+=( $test )
+            printf "[${RED}FAILED${NC}]: ${test}\n"
+        fi
         echo >> $LOG
     done
 fi
 
-percentage=$(echo "scale=2; $PASSED*100/$TOTAL" | bc)
-echo
-printf "[${YELLOW}REPORT${NC}]: Passed ${PASSED} tests | Failed ${FAILED} tests | Percent: ${percentage}%%" | tee -a $LOG
-echo
+if [[ $TOTAL -gt 0 ]]; then
+    percentage=$(echo "scale=2; ${#PASSED[@]}*100/$TOTAL" | bc)
+    echo | tee -a $LOG
+    printf "[${YELLOW}TEST REPORT${NC}]: Passed ${#PASSED[@]} tests | Failed ${#FAILED[@]} tests | Percent: ${percentage}%%" | tee -a $LOG
+    echo | tee -a $LOG
+else
+    echo "Did not run any tests." | tee -a $LOG
+fi
 
 rm -f $JSON
+
+# regression
+if [[ $REGRESSION -eq 1 ]]; then
+    if [[ ! -f $OLD_STATUS ]]; then
+        echo "Regression reports will be effective from the next run."
+        cp $NEW_STATUS $OLD_STATUS
+        exit 0
+    fi
+    STATUS=()
+    PASSED_NEW=()
+    FAILED_NEW=()
+    for test in $(cat $OLD_STATUS); do
+        STATUS+=( $test )
+    done
+    for test in ${PASSED[@]}; do
+        if [[ ! ${STATUS[@]} =~ $test ]]; then
+            PASSED_NEW+=( $test )
+        fi
+    done
+    for test in ${FAILED[@]}; do
+        if [[ ${STATUS[@]} =~ $test ]]; then
+            FAILED_NEW+=( $test )
+        fi
+    done
+    if [[ ${#PASSED_NEW[@]} -gt 0 ]]; then
+        echo | tee -a $LOG
+        printf "[${GREEN}NEW PASSING${NC}]" | tee -a $LOG
+        echo | tee -a $LOG
+        printf '%s\n' "${PASSED_NEW[@]}" | tee -a $LOG
+    fi
+    if [[ ${#FAILED_NEW[@]} -gt 0 ]]; then
+        echo | tee -a $LOG
+        printf "[${RED}NEW FAILING${NC}]" | tee -a $LOG
+        echo | tee -a $LOG
+        printf '%s\n' "${FAILED_NEW[@]}" | tee -a $LOG
+    fi
+    echo | tee -a $LOG
+    printf "[${YELLOW}REGRESSION REPORT${NC}]: Passed ${#PASSED_NEW[@]} new tests | Failed ${#FAILED_NEW[@]} new tests" | tee -a $LOG
+    echo | tee -a $LOG
+fi
--- tests/run_toml_tests.sh
+++ tests/run_toml_tests.sh
@@ -7,6 +7,8 @@ help()
     echo "          Type of tests to run: valid or invalid"
     echo "      -m <PATTERN> --match <PATTERN>"
     echo "          Match test filenames to PATTERN"
+    echo "      -r --regression"
+    echo "          Calculate and show regression report"
 }
 
 while [[ $# -gt 0 ]]; do
@@ -19,6 +21,10 @@ while [[ $# -gt 0 ]]; do
         -m|--match)
             MATCH=$2
             shift
+            shift
+            ;;
+        -r|--regression)
+            REGRESSION=1
             shift
             ;;
         -h|--help)
@@ -43,8 +49,8 @@ fi
 
 TOML_VERSION=1.0.0
 
-PASSED=0
-FAILED=0
+PASSED=()
+FAILED=()
 TOTAL=0
 
 RED='\033[0;31m'
@@ -54,9 +60,12 @@ NC='\033[0m'
 
 DIR=$(realpath $(realpath $0)/../..)
 LOG=$DIR/tests/toml-test.log
+OLD_STATUS=$DIR/tests/toml-test.status
+NEW_STATUS=$OLD_STATUS.new
 BINARY=$DIR/tests/test
 
 cd $DIR
+> $NEW_STATUS
 make test > $LOG
 
 # git clone https://github.com/toml-lang/toml-test.git tests/toml-tests
@@ -91,8 +100,14 @@ if [[ -z $TYPE || "$TYPE" == "valid" ]]; then
             json="${t%.toml}.json"
             diff <(jq --sort-keys . $json 2>>$LOG) <(jq --sort-keys . $JSON 2>>$LOG) >> $LOG
             r=$?
-            test $r -eq 0 && PASSED=$(( PASSED+1 )) || FAILED=$(( FAILED+1 ))
-            test $r -eq 0 && printf "[${GREEN}PASSED${NC}]: ${test}\n" || printf "[${RED}FAILED${NC}]: ${test}\n"
+            if [[ $r -eq 0 ]]; then
+                PASSED+=( $test )
+                printf "[${GREEN}PASSED${NC}]: ${test}\n"
+                echo $test >> $NEW_STATUS
+            else
+                FAILED+=( $test )
+                printf "[${RED}FAILED${NC}]: ${test}\n"
+            fi
             echo >> $LOG
         fi
     done
@@ -117,16 +132,66 @@ if [[ -z $TYPE || "$TYPE" == "invalid" ]]; then
             echo >> $LOG
             $BINARY < $TOML_TEST_DIR/$test >> $LOG 2>/dev/null
             r=$?
-            test $r -eq 1 && PASSED=$(( PASSED+1 )) || FAILED=$(( FAILED+1 ))
-            test $r -eq 1 && printf "[${GREEN}PASSED${NC}]: ${test}\n" || printf "[${RED}FAILED${NC}]: ${test}\n"
+            if [[ $r -eq 1 ]]; then
+                PASSED+=( $test )
+                printf "[${GREEN}PASSED${NC}]: ${test}\n"
+                echo $test >> $NEW_STATUS
+            else
+                FAILED+=( $test )
+                printf "[${RED}FAILED${NC}]: ${test}\n"
+            fi
             echo >> $LOG
         fi
     done
 fi
 
-percentage=$(echo "scale=2; $PASSED*100/$TOTAL" | bc)
-echo
-printf "[${YELLOW}REPORT${NC}]: Passed ${PASSED} tests | Failed ${FAILED} tests | Percent: ${percentage}%%" | tee -a $LOG
-echo
+if [[ $TOTAL -gt 0 ]]; then
+    percentage=$(echo "scale=2; ${#PASSED[@]}*100/$TOTAL" | bc)
+    echo | tee -a $LOG
+    printf "[${YELLOW}TEST REPORT${NC}]: Passed ${#PASSED[@]} tests | Failed ${#FAILED[@]} tests | Percent: ${percentage}%%" | tee -a $LOG
+    echo | tee -a $LOG
+else
+    echo "Did not run any tests." | tee -a $LOG
+fi
 
 rm -f $JSON
+
+# regression
+if [[ ! -z $REGRESSION && $REGRESSION -eq 1 ]]; then
+    if [[ ! -f $OLD_STATUS ]]; then
+        echo "Regression reports will be effective from the next run."
+        cp $NEW_STATUS $OLD_STATUS
+        exit 0
+    fi
+    STATUS=()
+    PASSED_NEW=()
+    FAILED_NEW=()
+    for test in $(cat $OLD_STATUS); do
+        STATUS+=( $test )
+    done
+    for test in ${PASSED[@]}; do
+        if [[ ! ${STATUS[@]} =~ $test ]]; then
+            PASSED_NEW+=( $test )
+        fi
+    done
+    for test in ${FAILED[@]}; do
+        if [[ ${STATUS[@]} =~ $test ]]; then
+            FAILED_NEW+=( $test )
+        fi
+    done
+    if [[ ${#PASSED_NEW[@]} -gt 0 ]]; then
+        echo | tee -a $LOG
+        printf "[${GREEN}NEW PASSING${NC}]" | tee -a $LOG
+        echo | tee -a $LOG
+        printf '%s\n' "${PASSED_NEW[@]}" | tee -a $LOG
+    fi
+    if [[ ${#FAILED_NEW[@]} -gt 0 ]]; then
+        echo | tee -a $LOG
+        printf "[${RED}NEW FAILING${NC}]" | tee -a $LOG
+        echo | tee -a $LOG
+        printf '%s\n' "${FAILED_NEW[@]}" | tee -a $LOG
+    fi
+    echo | tee -a $LOG
+    printf "[${YELLOW}REGRESSION REPORT${NC}]: Passed ${#PASSED_NEW[@]} new tests | Failed ${#FAILED_NEW[@]} new tests" | tee -a $LOG
+    echo | tee -a $LOG
+fi
